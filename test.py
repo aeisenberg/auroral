c173793e44e6d2e972e3a1d2f21ffb19897dc928
Vincent Therrien <vincentstherrien@gmail.com>
2024-12-29 20:21:55 -0500

    Parametrize

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 """A self-contained test for a DQN.
 
+The goal of the reinforcement learning task is for the agent (a red square) to
+reach a target (a green square). The environment is a 1D or 2D matrix.
+Configure the section labelled "PARAMETERS" to modify the test.
+
 Usage:
 
 $ python3 test.py
@@ -23,35 +27,39 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-GAME_SIZE = 16
-GAME_DIMENSIONS = 1
-LEARNING_RATE = 1e-3
-USE_CONVOLUTIONAL_LAYER = True
-N_FRAMES = 1
+# PARAMETERS
+DEVICE = "cuda"  # Use "cpu" if CUDA is not available.
+GAME_SIZE = 8  # Length of the environment.
+GAME_DIMENSIONS = 2  # Dimension of the environment, either 1 or 2.
+N_FRAMES= 4  # Number of game frames fed to the DQN.
+LEARNING_RATE = 1e-3  # Learning rate of the Adam optimizer
+USE_CONVOLUTIONAL_LAYER = True  # Apply a convolutional layer to the input of the DQN
+USE_MEMORY_REPLAY = True  # Use a memory replay buffer.
 
-N_EPISODES = 1500
-SCREEN_DIMENSION = (350, 128)
-N_STEPS = GAME_SIZE * 2
-BATCH_SIZE = 32
-TARGET_UPDATE_FREQUENCY = 1000
-EVALUATION_FREQUENCY = 1000
-MEMORY_SIZE = 10000
-GAMMA = 0.99
-REWARDS = {
+N_EPISODES = 1000  # Total number of episodes during training
+SCREEN_DIMENSION = (350, 256)  # Dimension of the screen used for visualization
+N_STEPS = GAME_SIZE * GAME_DIMENSIONS * 2  # Maximum number of steps in an episode
+BATCH_SIZE = 32  # Number of data points in a batch
+TARGET_UPDATE_FREQUENCY = 1000  # Update frequency of the target DQN in number of steps
+EVALUATION_FREQUENCY = 1000  # Evaluation frequency in number of steps
+EVALUATION_N_EPISODES = 100  # Number of episodes to test the DQN
+MEMORY_SIZE = 10000  # Size of the memory replay buffer
+GAMMA = 0.99  # Discount factor
+REWARDS = {  # Rewards given to the DQN for each event in the simulation
     "reach objective": 10,
     "move closer to objective": 1,
     "move away from objective": -1,
     "remain static": -0.5,
     "out of bound penalty": -1
 }
-N_ACTIONS = 4
+N_ACTIONS = 4  # Total number of actions
 
 
+# DNQ
 class DQN(nn.Module):
     def __init__(self, n):
         super(DQN, self).__init__()
         n2 = int(n / 2)
-        n4 = int(n / 4)
         # 1D
         self.conv1d = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
         self.network = nn.Sequential(
@@ -59,7 +67,16 @@ class DQN(nn.Module):
             nn.ReLU(),
             nn.Linear(n2, N_ACTIONS)
         )
+        # 1D Stacked
+        self.network_stacked = nn.Sequential(
+            nn.Linear(n * N_FRAMES, n),
+            nn.ReLU(),
+            nn.Linear(n, n2),
+            nn.ReLU(),
+            nn.Linear(n2, N_ACTIONS)
+        )
         # 2D
+        self.conv2d = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
         self.network2d = nn.Sequential(
             nn.Flatten(),
             nn.Linear(GAME_SIZE ** 2, GAME_SIZE * 2),
@@ -68,29 +85,67 @@ class DQN(nn.Module):
             nn.ReLU(),
             nn.Linear(GAME_SIZE, N_ACTIONS)
         )
+        self.network2d_stacked = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(GAME_SIZE ** 2 * N_FRAMES, GAME_SIZE ** 2),
+            nn.ReLU(),
+            nn.Linear(GAME_SIZE ** 2, GAME_SIZE),
+            nn.ReLU(),
+            nn.Linear(GAME_SIZE, N_ACTIONS)
+        )
         self.output = nn.Softmax(dim=0)
 
     def forward(self, x):
         if GAME_DIMENSIONS == 1:
             if USE_CONVOLUTIONAL_LAYER:
-                x = x.unsqueeze(1)
-                x = self.conv1d(x)
-                x = torch.flatten(x, start_dim=1)
-            return torch.sigmoid(self.network(x))
+                if N_FRAMES == 1:
+                    x = x.unsqueeze(1)
+                    x = self.conv1d(x)
+                    x = torch.flatten(x, start_dim=1)
+                    return torch.sigmoid(self.network(x))
+                else:
+                    x = x.view(x.size(0), -1)
+                    x = x.unsqueeze(1)
+                    x = self.conv1d(x)
+                    x = torch.flatten(x, start_dim=1)
+                    return torch.sigmoid(self.network_stacked(x))
+            else:
+                if N_FRAMES == 1:
+                    return torch.sigmoid(self.network(x))
+                else:
+                    x = x.view(x.size(0), -1)
+                    return torch.sigmoid(self.network_stacked(x))
         else:
-            return torch.sigmoid(self.network2d(x))
+            if USE_CONVOLUTIONAL_LAYER:
+                if N_FRAMES == 1:
+                    exit()
+                else:
+                    x = x.unsqueeze(2)
+                    batch_size, num_frames, _, _, _ = x.shape
+                    x = x.view(batch_size * num_frames, 1, x.size(3), x.size(4))
+                    x = self.conv2d(x)
+                    x = x.view(batch_size, num_frames, -1)
+                    x = x.view(batch_size, -1)
+                    return torch.sigmoid(self.network2d_stacked(x))
+            else:
+                if N_FRAMES == 1:
+                    return torch.sigmoid(self.network2d(x))
+                else:
+                    x = x.view(x.size(0), -1)
+                    return torch.sigmoid(self.network2d_stacked(x))
 
 
+# ENVIRONMENT
 screen = pygame.display.set_mode(SCREEN_DIMENSION)
 pygame.init()
 pygame.display.set_caption("Auroral")
 
 
-# ENVIRONMENT
 class Environment:
     def __init__(self, n, dimension):
         self.n = n
         self.dimension = dimension
+        self.grids = deque(maxlen=N_FRAMES)
         if dimension == 1:
             self.grid = np.zeros(n)
             self.objective = randint(0, n - 1)
@@ -107,6 +162,8 @@ class Environment:
                 if agent_position != self.objective:
                     self.agent_position = agent_position
                     break
+        for _ in range(N_FRAMES):
+            self.grids.append(self.grid.copy())
 
     def observe(self):
         if self.dimension == 1:
@@ -121,7 +178,12 @@ class Environment:
             self.grid = np.zeros((self.n, self.n))
             self.grid[self.agent_position[0], self.agent_position[1]] = 0.5
             self.grid[self.objective[0], self.objective[1]] = 1.0
-        return self.grid.copy()
+        if N_FRAMES == 1:
+            return self.grid.copy()
+        else:
+            self.grids.append(self.grid.copy())
+            grids = [g for g in self.grids]
+            return np.stack(grids, axis=0)
 
     def render(self, screen):
         normal = (255, 255, 255)
@@ -200,9 +262,9 @@ class Environment:
         return reward, done
 
 
-# MODEL
-policy_net = DQN(GAME_SIZE).to("cuda")
-target_net = DQN(GAME_SIZE).to("cuda")
+# TRAINING
+policy_net = DQN(GAME_SIZE).to(DEVICE)
+target_net = DQN(GAME_SIZE).to(DEVICE)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 optimizer = optim.Adam(policy_net.parameters(), lr=LEARNING_RATE)
@@ -215,7 +277,7 @@ def select_action(state, epsilon):
         action[randint(0, len(action) - 1)] = 1
         return action
     else:
-        state = torch.FloatTensor(np.stack(np.array([state]), axis=0)).to("cuda")
+        state = torch.FloatTensor(np.stack(np.array([state]), axis=0)).to(DEVICE)
         Q = policy_net(state)[0].tolist()
         return [1 if q == max(Q) else 0 for q in Q]
 
@@ -227,11 +289,11 @@ def optimize_model():
     batch = sample(memory, BATCH_SIZE)
     state_batch, action_batch, reward_batch, next_state_batch, done_batch = zip(*batch)
 
-    state_batch = torch.FloatTensor(np.stack(state_batch, axis=0)).to("cuda")
-    action_batch = torch.LongTensor(np.stack(action_batch, axis=0)).to("cuda")
-    reward_batch = torch.FloatTensor(np.stack(reward_batch, axis=0)).to("cuda")
-    next_state_batch = torch.FloatTensor(np.stack(next_state_batch, axis=0)).to("cuda")
-    done_batch = torch.FloatTensor(np.stack(done_batch, axis=0)).to("cuda")
+    state_batch = torch.FloatTensor(np.stack(state_batch, axis=0)).to(DEVICE)
+    action_batch = torch.LongTensor(np.stack(action_batch, axis=0)).to(DEVICE)
+    reward_batch = torch.FloatTensor(np.stack(reward_batch, axis=0)).to(DEVICE)
+    next_state_batch = torch.FloatTensor(np.stack(next_state_batch, axis=0)).to(DEVICE)
+    done_batch = torch.FloatTensor(np.stack(done_batch, axis=0)).to(DEVICE)
 
     # Compute Q-values for current states
     q_values = policy_net(state_batch)
@@ -264,13 +326,13 @@ def evaluate(slow_down = False):
     print("EVALUATING")
     exit_loop = False
     durations = []
-    for _ in range(100):
+    for _ in range(EVALUATION_N_EPISODES):
         env = Environment(GAME_SIZE, GAME_DIMENSIONS)
         for step in range(GAME_SIZE * 4):
             # Act
             state = env.observe()
             action = select_action(state, 0.0)
-            state = torch.FloatTensor(np.stack(np.array([state]), axis=0)).to("cuda")
+            state = torch.FloatTensor(np.stack(np.array([state]), axis=0)).to(DEVICE)
             Q = target_net(state)[0].tolist()
             action = [1 if q == max(Q) else 0 for q in Q]
             _, done = env.update(action)
